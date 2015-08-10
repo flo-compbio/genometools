64fadd309c0b5dac851c037fc2cae01c9d9bc495
Florian Wagner <florian.wagner@duke.edu>
2015-08-10 14:10:56 -0400

    update

--- genometools/genomic_features.py
+++ genometools/genomic_features.py
@@ -1,30 +1,124 @@
 from genometools import misc
 
+from HTSeq import GenomicPosition as GP, GenomicInterval as GI
+
 class GenomicFeature(object):
 	def __init__(self):
 		pass
 	
 	def __repr__(self):
-		return "<GenomicFeature>" %(self.gene)
+		return "<GenomicFeature>"
 	def __str__(self):
-		return "<GenomicFeature>" %(self.gene)
+		return "<GenomicFeature>"
 
-class ChipPeak(GenomicFeature):
-	def __init__(self,chrom,start,end,summit):
-		super(ChipPeak,self).__init__()
+	def __hash__(self):
+		return hash(repr(self))
+
+	def __eq__(self,other):
+		if self is other:
+			return True
+		elif type(self) != type(other):
+			return False
+		elif repr(self) == repr(other):
+			return True
+		else:
+			return False
+
+class LocationFeature(GenomicFeature):
+	def __init__(self,chrom,pos,strand='.'):
+		GenomicFeature.__init__(self)
+		assert strand in ['.','-','+']
+		self.loc = GP(chrom,pos,strand)
+
+	@property
+	def chrom(self):
+		return self.loc.chrom
+
+	@property
+	def pos(self):
+		return self.loc.pos
+
+	@property
+	def strand(self):
+		return self.loc.strand
+
+	def __repr__(self):
+		return '<LocationFeature (loc:"%s")>' %(repr(self.loc))
+
+	def __str__(self):
+		return '<LocationFeature at "%s">' %(str(self.loc))
+
+	def set_location(self,chrom,pos,strand='.'):
+		self.loc = GP(chrom,pos,strand)
+
+
+class IntervalFeature(GenomicFeature):
+	def __init__(self,chrom,start,end,strand='.'):
 		assert start < end
-		assert summit < (end-start)
-		self.chrom = chrom
-		self.start = start
-		self.end = end
-		self.summit = summit
+		assert strand in ['.','-','+']
+		self.iv = GI(chrom,start,end,strand)
+
+	@property
+	def chrom(self):
+		return self.iv.chrom
+
+	@property
+	def start(self):
+		return self.iv.start
+
+	@property
+	def end(self):
+		return self.iv.end
+
+	@property
+	def strand(self):
+		return self.iv.strand
 
 	@property
 	def length(self):
-		return self.end - self.start
+		return self.iv.length
+
+	def __repr__(self):
+		return '<IntervalFeature (iv:"%s")>' %(repr(self.iv))
+
+	def __str__(self):
+		return '<IntervalFeature with interval "%s">' %(str(self.iv))
+
+	def set_interval(self,chrom,start,end,strand='.'):
+		self.iv = GI(chrom,start,end,strand)
+
+
+class GeneFeature(GenomicFeature):
+	def __init__(self,gene):
+		self.gene = gene
+
+	def __repr__(self):
+		return '<GeneFeature (gene:"%s")>' %(self.gene)
+
+	def __str__(self):
+		return '<GeneFeature of gene "%s">' %(self.gene)
+
+
+class TranscriptFeature(GeneFeature):
+	def __init__(self,gene,transcript):
+		GeneFeature.__init__(self,gene)
+		self.transcript = transcript
+
+	def __repr__(self):
+		return '<TranscriptFeature (gene:"%s", transcript:"%s")>' %(self.gene,self.transcript)
+
+	def __str__(self):
+		return '<TranscriptFeature of gene "%s", transcript "%s">' %(self.gene,self.transcript)
+
+
+class ChipPeak(IntervalFeature):
+	def __init__(self,chrom,start,end,summit):
+		IntervalFeature.__init__(self,chrom,start,end)
+		assert summit < (end-start)
+		self.summit = summit
 
 	@classmethod
-	def read_ucsc_narrowpeak_file(cls,narrowpeak_file):
+	def read_ucsc_narrowpeaks(cls,narrowpeak_file):
 		data = misc.read_all(narrowpeak_file)
 		peaks = []
 		for d in data:
@@ -42,150 +136,23 @@ class ChipPeak(GenomicFeature):
 		return peaks
 
 	def __repr__(self):
-		return "<Peak %s: %d-%d (%d bp), summit=%d)>" %(self.chrom,self.start,self.end,self.length,self.summit)
+		return "<ChipPeak (chrom:%s,start:%d,end:%d,summit:%d)>" %(self.chrom,self.start,self.end,self.summit)
+
 	def __str__(self):
-		return "<Peak on chromosome '%s' (%d - %d), length = %d bp, summit @ %d>" \
+		return "<ChipPeak on chromosome '%s' (%d - %d), length = %d bp, summit @ %d>" \
 				%(self.chrom,self.start,self.end,self.length,self.summit+1)
-		
 
-class Peak(GenomicFeature):
 
-	def __init__(self,chromosome,start,end,summit):
-		super(Peak,self).__init__()
-		assert start < end
-		assert summit < (end-start)
-		self.chromosome = chromosome
-		self.start = start
-		self.end = end
-		self.summit = summit
-
-	def get_length(self):
-		return self.end - self.start
+class TSS(TranscriptFeature,LocationFeature):
+	def __init__(self,gene,transcript,chrom,pos,strand):
+		TranscriptFeature.__init__(self,gene,transcript)
+		LocationFeature.__init__(self,chrom,pos,strand)
 
 	def __repr__(self):
-		return "<Peak %s: %d-%d (%d)>" %(self.chromosome,self.start,self.end,self.summit)
+		return '<TSS (gene:"%s", transcript:"%s", strand:%d)>' %(self.gene,self.transcript,self.strand)
 	def __str__(self):
-		return "<Peak on chromosome '%d' (%d - %d), length = %d bp, summit @%d>" \
-				%(self.chromosome,self.start,self.end,self.get_length(),self.summit+1)
+		return '<TSS of gene "%s"/transcript "%s", on "%s" strand>' %(self.gene,self.transcript,self.strand)
 
-	def __eq__(self,other):
-		if type(self) != type(other):
-			return False
-
-		elif self is other:
-			return True
-
-		elif repr(self) == repr(other):
-			return True
-
-		else:
-			return False
-		"""			
-		elif self.chromosome == other.chromosome \
-			and self.start == other.start \
-			and self.end == other.end \
-			and self.summit == other.summit:
-				return True
-
-		else:
-			return False
-		"""
-
-	def __hash__(self):
-		return hash(repr(self))
-
-"""
-class MACSPeak(GenomicFeature):
-	def __init__(self,data):
-		super(MACSPeak,self).__init__()
-		self.data = tuple(data) # tuple with coordinates etc., just like in MACS peak file
-
-	def __repr__(self):
-		return "<Feature gene:'%s'>" %(self.gene)
-	def __str__(self):
-		return "<Feature of gene '%s'>" %(self.gene)
-
-	def __eq__(self,other):
-		if isinstance(other,self.__class__):
-			return (self.data == other.data)
-		else:
-			return False
-	def __ne__(self,other):
-		return not self.__eq__(other)
-
-	def __hash__(self):
-		return hash(self.data)
-"""
-
-class GeneFeature(GenomicFeature):
-	def __init__(self,gene):
-		super(GeneFeature,self).__init__()
-		self.gene = gene
-
-	def __repr__(self):
-		return "<Feature gene:'%s'>" %(self.gene)
-	def __str__(self):
-		return "<Feature of gene '%s'>" %(self.gene)
-
-	def __hash__(self):
-		return hash(repr(self))
-
-	def __eq__(self,other):
-		if self is other:
-			return True
-		elif type(self) != type(other):
-			return False
-		elif repr(self) == repr(other):
-			return True
-		else:
-			return False
-
-class DirectedGeneFeature(GeneFeature):
-	def __init__(self,gene,direction):
-		super(DirectedGeneFeature,self).__init__(gene)
-		self.dir = direction
-
-	def __repr__(self):
-		return "<DirectedGeneFeature (gene:'%s', direction %d)>" %(self.gene,self.dir)
-	def __str__(self):
-		return "<DirectedGeneFeature of gene '%s', direction %d>" %(self.gene,self.dir)
-
-	def __eq__(self,other):
-		if isinstance(other,self.__class__):
-			return (self.gene == other.gene and self.dir == other.dir)
-		else:
-			return False
-	def __ne__(self,other):
-		return not self.__eq__(other)
-
-	def __hash__(self):
-		return hash((self.gene,self.dir))
-
-class TSS(DirectedGeneFeature):
-	def __init__(self,gene,transcript,direction):
-		super(TSS,self).__init__(gene,direction)
-		self.transcript = transcript
-
-	def __repr__(self):
-		return "<TSS (gene:'%s', transcript:'%s', direction %d)>" %(self.gene,self.transcript,self.dir)
-	def __str__(self):
-		return "<TSS of gene '%s'/transcript '%s', direction %d>" %(self.gene,self.transcript,self.dir)
-
-	def __eq__(self,other):
-		if not isinstance(other,self.__class__):
-			return False
-		elif other is self:
-			return True
-		elif repr(self) == repr(other):
-			return True
-			#return (self.gene == other.gene and self.transcript == other.transcript and self.dir == other.dir)
-		else:
-			return False
-	def __ne__(self,other):
-		return not self.__eq__(other)
-
-	def __hash__(self):
-		return hash((self.gene,self.transcript,self.dir))
 
 def convert_chrom_from_ensembl(c):
 	chrom = c
