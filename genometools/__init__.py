64aa744c8160d38e08da5fb24589ed5b1808f96b
Florian Wagner <florian.wagner@duke.edu>
2015-12-10 09:22:13 -0500

    fix

--- genometools/__init__.py
+++ genometools/__init__.py
@@ -2,6 +2,10 @@ import pkg_resources
 
 __version__ = pkg_resources.require('genometools')[0].version
 
-from genometools import misc, gtf, ensembl, rnaseq, sra
+
+
+import genometools.misc
+
+from genometools import gtf, ensembl, rnaseq, sra
 
 __all__ = []
