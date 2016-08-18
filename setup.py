60157c0da741c8d4b58b4a7b0070bab629e49539
Florian Wagner <florian.wagner@duke.edu>
2016-08-18 15:02:28 -0400

    fix docs [ci skip]

--- setup.py
+++ setup.py
@@ -38,6 +38,10 @@ install_requires = [
     'xmltodict>=0.10.1, <1',
     'requests>=2.9.1, <3',
     'ftputil>=3.3.1, <4',
+    'pandas>=0.18, <1',
+    'numpy>=1.8, <2',
+    'plotly>=1.9.6, <2',
+    'xlmhg>=2.2.0, <3',
 ]
 
 # do not require installation if built by ReadTheDocs
@@ -45,11 +49,7 @@ install_requires = [
 if 'READTHEDOCS' not in os.environ or \
         os.environ['READTHEDOCS'] != 'True':
     install_requires.extend([
-        'numpy>=1.8, <2',
-        'pandas>=0.18, <1',
         'scipy>=0.14, <1',
-        'plotly>=1.9.6, <2',
-        'xlmhg>=2.2.0, <3',
     ])
 
 # get long description from file
