51d3ae47811dc8f03eac7f964441a35816064717
Florian Wagner <florian.wagner@duke.edu>
2017-03-04 13:34:32 -0500

    fix windows download

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from os import path
 
 root = 'genometools'
 name = 'genometools'
-version = '0.2.4'
+version = '0.2.5'
 
 here = path.abspath(path.dirname(__file__))
 description = 'GenomeTools: A Python Framework for Analyzing Genomic Data.'
